2f03bdde194ac3c72b10307995bb4e84e57b503b
Janzen <62186185+janzenwork@users.noreply.github.com>
2020-06-27 20:22:22 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Knuth
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] nums = { 0, 1, 2, 3, 4, 5, 3, 4, 5 };
+            Shuffle(nums);
+        }
+
+        public static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+            int[] shuffledArray = array;
+            int temp = 0;
+
+            for (int i = 0; i < shuffledArray.Length; i++)
+            {
+                temp = shuffledArray[i];
+                int j = random.Next(i, shuffledArray.Length);
+                shuffledArray[i] = shuffledArray[j];
+                shuffledArray[j] = temp;
+            }
+
+            foreach (var item in shuffledArray) { Console.Write(item + ", "); }
+        }
+    }
+}
